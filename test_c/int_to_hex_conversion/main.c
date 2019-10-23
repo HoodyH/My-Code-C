467517e573a7fe4280ae19f6d8ec2bde860276e2
snot <not.simone96@gmail.com>
2019-10-23 10:33:01 +0200

    some optimizations

--- test_c/int_to_hex_conversion/main.c
+++ test_c/int_to_hex_conversion/main.c
@@ -3,20 +3,20 @@
 #include <stdint.h>
 
 
-void convert(int16_t from, int16_t to, char * s);
-int16_t b2_shift(int16_t val);
+void convert(int8_t from, int8_t to, char * s);
+int8_t b2_shift(int8_t val);
 
 int main(int argc, char **argv)
 {
-    uint16_t data[10] = {0,0,0,6,0,8,0,9,1,0};
+    int8_t data[10] = {0,0,0,6,0,8,0,9,1,0};
     char str_data[12];
 
-    int16_t len = sizeof(data)/sizeof(data[0]);
-    int16_t i_str_data = 0;
-    int16_t jump_zero_at_start = 0;
+    int8_t len = sizeof(data)/sizeof(data[0]);
+    int8_t i_str_data = 0;
+    int8_t jump_zero_at_start = 0;
     printf("len = %d\n", len);
 
-    for(int16_t i_data = 0; i_data <= len-1; i_data++){
+    for(int8_t i_data = 0; i_data <= len-1; i_data++){
         if(0 != data[i_data] || 1 == jump_zero_at_start){
             jump_zero_at_start = 1;
             str_data[i_str_data] = (char)(data[i_data] + '0');
@@ -31,7 +31,7 @@ int main(int argc, char **argv)
     return 0;
 }
 
-void convert(int16_t from, int16_t to, char * s)
+void convert(int8_t from, int8_t to, char * s)
 {
     
     //only do base 2 to base 36 (digit represented by characters 0-Z)"
@@ -40,20 +40,20 @@ void convert(int16_t from, int16_t to, char * s)
     }
     
     //convert string to an array of integer digits representing number in base:from
-    int16_t static il = sizeof(s)/sizeof(s[0]) - 1; // remove the \0 in the counting
+    int8_t static il = sizeof(s)/sizeof(s[0]) - 1; // remove the \0 in the counting
 
-    int16_t fs[il];
-    int16_t k = 0;
-    for (int16_t i = il - 1; i >= 0; i--)
+    int8_t fs[il];
+    int8_t k = 0;
+    for (int8_t i = il - 1; i >= 0; i--)
     {   
         printf("c %c\n", s[i]);
         if (s[i] >= '0' && s[i] <= '9') {
-            fs[k++] = (int16_t)(s[i] - '0');
+            fs[k++] = (int8_t)(s[i] - '0');
         }
         else
         {
             if (s[i] >= 'A' && s[i] <= 'Z') { 
-                fs[k++] = 10 + (int16_t)(s[i] - 'A'); 
+                fs[k++] = 10 + (int8_t)(s[i] - 'A'); 
             }
             else return; //only allow 0-9 A-Z characters
         }
@@ -110,14 +110,15 @@ void convert(int16_t from, int16_t to, char * s)
         }
     }
     
-    for (int16_t i = il - 1; i >= 0; i--)
+    for (int8_t i = il - 1; i >= 0; i--)
     {   
         printf("cums %d\n", cums[i]);
     }
     
-    //convert the output to string format (digits 0,to-1 converted to 0-Z characters) 
+    //convert the output to string format (digits 0, to-1 converted to 0-Z characters) 
     char sout[8] = "/0";
     int16_t first = 0; //leading zero flag
+    printf("ol = %d\n", ol);
     for (int i = ol ; i >= 0; i--)
     {
         if (cums[i] != 0) { first = 1; }
@@ -129,11 +130,12 @@ void convert(int16_t from, int16_t to, char * s)
             sout[i] = (char)(cums[i] + 'A'-10); 
         }
     }
+    sout[7] = '/0';
     printf("hex data = %s\n", sout);
 }
 
-int16_t b2_shift(int16_t val){
-    int16_t shifted_val = (val & 1) << 7 | 
+int8_t b2_shift(int8_t val){
+    int8_t shifted_val = (val & 1) << 7 | 
         (val & 2) << 5 | 
         (val & 4) << 3 | 
         (val & 8) << 1 | 
